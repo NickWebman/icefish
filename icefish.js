63c28f54e07a4b257775ecae54b0bb2c7091c9b1
NickWebman <nick.w.anthony@gmail.com>
2016-01-20 15:43:10 -0500

    wrote some words. did some stuff.

--- icefish.js
+++ icefish.js
@@ -44,6 +44,8 @@ $(document).ready(function(){
 	$(".name .value").text(localStorage.name);
 	$(".weight .value").text(localStorage.weight);	
 	
+	var done = false;
+	
 	var event = false;
 	var eventName = "";
 	var eventMath = 0;
@@ -51,12 +53,26 @@ $(document).ready(function(){
 	var eventCastListMessage = "";
 	var eventXp = 0;
 	
+	var shamePoints = 0;
+	var drunkPoints = 0;
+	var highPoints = 0;
+	var gulliblePoints = 0;
+	var unsuccessfulPoints = 0;
+	var despicablePoints = 0;
+	var unstablePoints = 0;
+	var meatHeadPoints = 0;
+	var doucheyPoints = 0;
+	var sadisticPoints = 0;
+	var kindPoints = 0;
+	var mentallyIllPoints = 0;
+	var coldBloodedPoints = 0;
+	
 	var morale = 100;
 	var hunger = 0;
 	
 	var insanityPoints = 0;
 	var niceGuyPoints = 0;
-	var dickPoints = 0;
+	var despicablePoints = 0;
 	
 	var casts = 0;
 	var fishBite = false;
@@ -85,7 +101,24 @@ $(document).ready(function(){
 	$("#CastButton").prop("enabled", true);
 	$("#SetHookButton").prop("disabled", true);
 	$("#ReelInButton").prop("disabled", true);
-	
+
+
+	jQuery.fn.animateAuto = function(prop, speed, callback){
+		var elem, height, width;
+		return this.each(function(i, el){
+			el = jQuery(el), elem = el.clone().css({"height":"auto","width":"auto"}).appendTo("body");
+			height = elem.css("height"),
+			width = elem.css("width"),
+			elem.remove();
+			
+			if(prop === "height")
+				el.animate({"height":height}, speed, callback);
+			else if(prop === "width")
+				el.animate({"width":width}, speed, callback);  
+			else if(prop === "both")
+				el.animate({"width":width,"height":height}, speed, callback);
+		});  
+	}	
 	
 	// functions
 
@@ -106,6 +139,40 @@ $(document).ready(function(){
 		$(".level-message .value").show("slow").delay(2000).slideUp("slow");		
 	}
 
+	function addTraits() {
+		if (despicablePoints > 0 && $('.stuff .the-stuff .despicable').length == 0){
+			$(".stuff .the-stuff").append('<div class="despicable"><span class="label">Despicable</span><span class="value"> x<span>'+ despicablePoints +'</span></span></div>')
+		} if (unsuccessfulPoints > 0 && $('.stuff .the-stuff .unsuccessful').length == 0){
+			$(".stuff .the-stuff").append('<div class="unsuccessful"><span class="label">Unsuccessful</span><span class="value"> x<span>'+ unsuccessfulPoints +'</span></span></div>')
+		} if (unstablePoints > 0 && $('.stuff .the-stuff .unstable').length == 0){
+			$(".stuff .the-stuff").append('<div class="unstable"><span class="label">Unstable</span><span class="value"> x<span>'+ unstablePoints +'</span></span></div>')
+		} if (doucheyPoints > 0 && $('.stuff .the-stuff .douchey').length == 0){
+			$(".stuff .the-stuff").append('<div class="douchey"><span class="label">Douchey</span><span class="value"> x<span>'+ doucheyPoints +'</span></span></div>')
+		} if (sadisticPoints > 0 && $('.stuff .the-stuff .sadistic').length == 0){
+			$(".stuff .the-stuff").append('<div class="sadistic"><span class="label">Sadistic</span><span class="value"> x<span>'+ sadisticPoints +'</span></span></div>')
+		} if (kindPoints > 0 && $('.stuff .the-stuff .kind').length == 0){
+			$(".stuff .the-stuff").append('<div class="kind"><span class="label">Kind</span><span class="value"> x<span>'+ kindPoints +'</span></span></div>')
+		} if (mentallyIllPoints > 0 && $('.stuff .the-stuff .mentally-ill').length == 0){
+			$(".stuff .the-stuff").append('<div class="mentally-ill"><span class="label">Mentally Ill</span><span class="value"> x<span>'+ mentallyIllPoints +'</span></span></div>')
+		} if (coldBloodedPoints > 0 && $('.stuff .the-stuff .cold-blooded').length == 0){
+			$(".stuff .the-stuff").append('<div class="cold-blooded"><span class="label">Cold Blooded</span><span class="value"> x<span>'+ coldBloodedPoints +'</span></span></div>')	
+		}						
+
+		// populate the stuff values
+		
+		$(".stuff .the-stuff .despicable .value span").text(despicablePoints);
+		$(".stuff .the-stuff .unsuccessful .value span").text(unsuccessfulPoints);
+		$(".stuff .the-stuff .wild-card .value span").text(unstablePoints);
+		$(".stuff .the-stuff .douchey .value span").text(doucheyPoints);
+		$(".stuff .the-stuff .sadistic .value span").text(sadisticPoints);
+		$(".stuff .the-stuff .kind .value span").text(kindPoints);
+		$(".stuff .the-stuff .mentally-ill .value span").text(mentallyIllPoints);						
+		$(".stuff .the-stuff .cold-blooded .value span").text(coldBloodedPoints);	
+
+		totalXp += eventXp;
+		$(".xp .value").text(totalXp);		
+	}	
+	
 	// clicks
 	
 	$("#CastButton").click(function(){		
@@ -312,6 +379,8 @@ $(document).ready(function(){
 				eventName = "pregnant";
 			}			
 			
+			eventName = "mermaid";
+			
 			switch (eventName) {
 				case "pregnant":
 					
@@ -319,52 +388,161 @@ $(document).ready(function(){
 					$(".level-message .value").show("slow");
 
 					$(".answers input").click(function(){
-
 						if ($(this).val() == "a"){
 							$(".status .value").html('That was nice of you <input id="KeepFishingButton" type="submit" class="button" value="Continue fishing">');
-							$(".status .value").show("slow");							
-							$(".status .value input").show("slow").css('display', 'block');
+							$(".status .value").show("slow").bind("click", function(e){
+								$(".status .value").animateAuto("height", 1000); 
+							});						
+							$(".status .value input").show(1000).css('display', 'block');
 							$(".answers input").prop("disabled", true);
 							$(".answers input").not(this).addClass("disabled");								
 							eventXp = 80;
 							eventCastListMessage = "You caught a pregnant fish. You threw her back because you're a cool person.";
-							niceGuyPoints = niceGuyPoints + 1;
-							morale = morale + 1;
+							kindPoints += 1;
+							morale += 1;
 							
 						} else if ($(this).val() == "b"){
 							$(".status .value").html("You sit the fish down and explain the concept of the food chain. When the fish responds \"That's cool. Where am I on the food chain?\", you look her in the eye and promptly bite into her face. <input id='KeepFishingButton' type='submit' class='button' value='Continue fishing'>");							
-							//$(".status .value").hide("slow");
-							$(".status .value .inner").show("slow");		
-							$(".status .value input").show("slow").css('display', 'block');
+							$(".status .value").show("slow").bind("click", function(e){
+								$(".status .value").animateAuto("height", 1000); 
+							});		
+							$(".status .value input").show(1000).css('display', 'block');
 							$(".answers input").prop("disabled", true);									
 							$(".answers input").not(this).addClass("disabled");								
 							eventXp = 80;							
 							eventCastListMessage = "You caught a pregnant fish. You explained the food chain and then ate her and her babies."
-							dickPoints = dickPoints + 1;
+							despicablePoints += 1;
+							coldBloodedPoints += 1;
+							hunger += -10;
+							morale += 1;
+							
+						} else if ($(this).val() == "c"){
+							$(".status .value").html("Why would you do that? No, seriously. Are you OK? <input id='KeepFishingButton' type='submit' class='button' value='Continue fishing'>");							
+							$(".status .value").show("slow").bind("click", function(e){
+								$(".status .value").animateAuto("height", 1000); 
+							});		
+							$(".status .value input").show(1000).css('display', 'block');
+							$(".answers input").prop("disabled", true);									
+							$(".answers input").not(this).addClass("disabled");								
+							eventXp = -50;
+							eventCastListMessage = "You caught a pregnant fish. You chose to stab her for no reason. This is why nobody likes you. You can't actually lose at this game, but you should.";
+							despicablePoints += 1;
+							doucheyPoints += 1;							
+							mentallyIllPoints += 1;
+							coldBloodedPoints += 1;							
+							unstablePoints += 1;
+						}
+
+					addTraits();
+					keepFishing();						
+						
+					});
+
+					break;
+				
+				case "mermaid":
+						
+					$(".level-message .value").html("<span class='special'>This fish seems different...</span><div class='inner'>Much to your surprise, a beautiful mermaid emerges from the fishing hole. Disoriented, she pleads, \"If you let me go, I will grant you one wish\". Intrigued, you respond... </div><div class='answers'><label><input type='radio' name='mermaid' value='a'>I wish for whiskey.</label><label><input type='radio' name='mermaid' value='b'>I wish for world peace.</label><label><input type='radio' name='mermaid' value='c'>I wish we would bang.</div></label>");
+					$(".level-message .value").show("slow");
+
+					$(".answers input").click(function(){
+						if ($(this).val() == "a"){
+							$(".status .value").html('"Oh cool. I have a drinking problem, too.", she responds as she retrieves a bottle marked "xxx" from her purse (this mermaid has a purse). The two of you sit down, share drinks, and tell tales of your polarizing worlds. Now laughing, you meet eachother\'s gaze and sheepishly smile. As passion takes over, you resign to the soothing effects of whiskey, and dive balls-deep into mermaid vagina. <p>Although mermaid boobs are pretty great, their lower-half is more fish than human. In other words, you stuck your penis in a fish vagina.</p> <p>You finish anyway.</p> <input id="KeepFishingButton" type="submit" class="button" value="Continue fishing">');
+							$(".status .value").hide().show("slow");			
+							$(".status .value input").show(1000).css('display', 'block');
+							$(".answers input").prop("disabled", true);
+							$(".answers input").not(this).addClass("disabled");								
+							eventXp = 80;
+							eventCastListMessage = "You caught a mermaid, got drunk with her, and had weird mermaid sex.";
+							kindPoints += 1;
+							morale += 1;
+							
+						} else if ($(this).val() == "b"){
+							$(".status .value").html('In a pretentious tone, you proudly announce "I wish for world peace". <p>Smirking, she says "I\'m a mermaid, not a genie. Idiot.", and hurriedly swims away.</p>  <input id="KeepFishingButton" type="submit" class="button" value="Continue fishing">');							
+							$(".status .value").hide().show("slow");
+							$(".status .value input").show(1000).css('display', 'block');
+							$(".answers input").prop("disabled", true);									
+							$(".answers input").not(this).addClass("disabled");								
+							eventXp = 80;							
+							eventCastListMessage = "You caught a mermaid who convinced you she could grant wishes. She couldn't."
+							gulliblePoints += 1;
+							
+						} else if ($(this).val() == "c"){
+							$(".status .value").html('"Mmmm ok big boy, but you must promise to keep your eyes shut, or else you\'ll turn into a merman", she responds flirtatiously. With a lack of knowledge on interspecies relations, you fear something\'s awry, yet don\'t know enough to dispute her claim. It\'s been a slow year, so you close your eyes and wish for the best. <p>It\'s cold and it\'s weird, but you finish anyway. You finally look down to find a cat fish latched onto your dong. Shame sinks in as you quickly realize you\'ve been had.</p> <input id="KeepFishingButton" type="submit" class="button" value="Continue fishing">');							
+							$(".status .value").hide().show("slow");
+							$(".status .value input").show(1000).css('display', 'block');
+							$(".answers input").prop("disabled", true);									
+							$(".answers input").not(this).addClass("disabled");								
+							eventXp = -50;
+							eventCastListMessage = "Wow. OK. So, you accidentally banged a fish...";
+							despicablePoints = despicablePoints + 1;
+
+						}
+						
+						addTraits();
+						keepFishing();
+					
+					});				
+					
+					break;
+
+				case "fall-in":
+						
+					$(".level-message .value").html("<span class='special'>This fish seems different...</span><div class='inner'>Blah blah: \"blahblahblah\" </div><div class='answers'><label><input type='radio' name='pregnant' value='a'>Ok. I won't eat you.</label><label><input type='radio' name='pregnant' value='b'>Meh. I'm still going to eat you.</label><label><input type='radio' name='pregnant' value='c'>Stab her</div></label>");
+					$(".level-message .value").show("slow");
+
+					$(".answers input").click(function(){
+						if ($(this).val() == "a"){
+							$(".status .value").html('That was nice of you <input id="KeepFishingButton" type="submit" class="button" value="Continue fishing">');
+							$(".status .value").hide().show("slow");						
+							$(".status .value input").show(1000).css('display', 'block');
+							$(".answers input").prop("disabled", true);
+							$(".answers input").not(this).addClass("disabled");								
+							eventXp = 80;
+							eventCastListMessage = "You caught a pregnant fish. You threw her back because you're a cool person.";
+							kindPoints = kindPoints + 1;
+							morale = morale + 1;
+							
+						} else if ($(this).val() == "b"){
+							$(".status .value").html("You sit the fish down and explain the concept of the food chain. When the fish responds \"That's cool. Where am I on the food chain?\", you look her in the eye and promptly bite into her face. <input id='KeepFishingButton' type='submit' class='button' value='Continue fishing'>");							
+							$(".status .value").hide().show("slow");
+							$(".status .value input").show(1000).css('display', 'block');
+							$(".answers input").prop("disabled", true);									
+							$(".answers input").not(this).addClass("disabled");								
+							eventXp = 80;							
+							eventCastListMessage = "You caught a pregnant fish. You explained the food chain and then ate her and her babies."
+							despicablePoints = despicablePoints + 1;
+							coldBloodedPoints = coldBloodedPoints + 1;
 							hunger = hunger + -10;
 							morale = morale + 1;
 							
 						} else if ($(this).val() == "c"){
-							$(".status .value").html("Why would you do that? No, seriously. This is only a game, but what the fuck? Are you OK? <input id='KeepFishingButton' type='submit' class='button' value='Continue fishing'>");							
-							$(".status .value").show("slow");
-							$(".status .value input").show("slow").css('display', 'block');
+							$(".status .value").html("Why would you do that? No, seriously. Are you OK? <input id='KeepFishingButton' type='submit' class='button' value='Continue fishing'>");							
+							$(".status .value").hide().show("slow");
+							$(".status .value input").show(1000).css('display', 'block');
 							$(".answers input").prop("disabled", true);									
 							$(".answers input").not(this).addClass("disabled");								
 							eventXp = -50;
-							eventCastListMessage = "You caught a pregnant fish. You chose to stab her for no reason. May God have mercy on your soul";
-							dickPoints = dickPoints + 1;
-							insanityPoints = insanityPoints + 1;
+							eventCastListMessage = "You caught a pregnant fish. You chose to stab her for no reason. This is why nobody likes you.";
+							despicablePoints = despicablePoints + 1;
+							doucheyPoints = doucheyPoints + 1;							
+							mentallyIllPoints = mentallyIllPoints + 1;
+							coldBloodedPoints = coldBloodedPoints + 1;							
+							unstablePoints = unstablePoints + 1;
 						}
 						
+						addTraits();
 						keepFishing();
-						
-					});
+					
+					});				
 					
 					break;
 			}
 			
 
 
+						
+						
 			
 			
 		} else {		
